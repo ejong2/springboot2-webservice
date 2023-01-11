368d8a59b242aea622f1d9c524ecee1f827b83ff
Lacoruna <kki9606@gmail.com>
2023-01-11 16:06:13 +0900

    Can't Connect to MYSQL from TCP Server

--- JunHyeock/TCP_Server_Single/TCP_Server_Single/main.cpp
+++ JunHyeock/TCP_Server_Single/TCP_Server_Single/main.cpp
@@ -4,14 +4,25 @@
 #include <stdlib.h>
 
 #include <WinSock2.h>
+#include "mysql-connector-c++-8.0.31-winx64/include/jdbc/cppconn/driver.h"
+#include "mysql-connector-c++-8.0.31-winx64/include/jdbc/mysql_connection.h"
+#include "mysql-connector-c++-8.0.31-winx64/include/jdbc/cppconn/prepared_statement.h"
+#include "mysql-connector-c++-8.0.31-winx64/include/jdbc/cppconn/exception.h"
 
-#include "mysql-connector/include/jdbc/mysql_connection.h"
-//#include "mysql-connector/include/jdbc/cppconn/driver.h"
-#include "mysql-connector/include/jdbc/cppconn/exception.h"
-#include "mysql-connector/include/jdbc/cppconn/prepared_statement.h"
+
+//#include "mysql_connection.h"
+//#include <cppconn/driver.h>
+//#include <cppconn/exception.h>
+//#include <cppconn/prepared_statement.h>
+
+
 
 #pragma comment(lib, "ws2_32.lib")
-#pragma comment(lib, "mysql-connector/lib64/vs14/debug/mysqlcppconn.lib")
+//#pragma comment(lib, "mysql-connector/lib64/vs14/debug/mysqlcppconn.lib")
+
+
+//Linker
+
 
 
 
@@ -39,6 +50,19 @@ sql::PreparedStatement* pstmt;
 int main()
 {
 
+	//try
+	//{
+	//	driver = get_driver_instance();
+	//	con = driver->connect(server, username, password);
+	//}
+	//catch (sql::SQLException e)
+	//{
+	//	cout << "Could not connect to server. Error message: " << e.what() << endl;
+	//	system("pause");
+	//	exit(1);
+	//}
+
+
 
 
 	packet mypack;
@@ -59,7 +83,8 @@ int main()
 	ServerSocket = socket(AF_INET, SOCK_STREAM, 0);
 	memset(&ServerSocketAddr, 0, sizeof(ServerSocketAddr));
 
-	ServerSocketAddr.sin_addr.S_un.S_addr = INADDR_ANY;
+	//ServerSocketAddr.sin_addr.S_un.S_addr = INADDR_ANY;
+	ServerSocketAddr.sin_addr.S_un.S_addr = inet_addr("127.0.0.1");
 	ServerSocketAddr.sin_family = PF_INET;
 	ServerSocketAddr.sin_port = htons(7476);
 
@@ -68,17 +93,52 @@ int main()
 
 	cout << "now Listenning..." << endl;
 	int ClientSockAddrLength = sizeof(ClientSockAddr);
+	
 
 	ClientSocket = accept(ServerSocket, reinterpret_cast<sockaddr*>(&ClientSockAddr), &ClientSockAddrLength);
 	cout << "======accept======" << endl;
-
+	
 	//char Buffer[32] = { 0, };
 
 	while (0)
 	{
+		recv(ClientSocket, (char*)&mypack, sizeof(mypack), 0);
+		switch (mypack.header)
+		{
+		case 1:
+			//sign in
+			//SQL 들어가서 ID중복인지 검사
+			//없으면 -> 생성
+			//send()
+			//있으면 -> 비생성
+			//send()
+			break;
+		case 2:
+			//log in
+			//SQL 들어가서 ID있는지 검사
+			// 없으면 -> 실패
+			// send()
+			// 있으면 -> 해당 ID와 column Password가 일치하는지 검사
+			// 일치하지않으면 -> 실패
+			// send()
+			// 일치하면 -> 성공
+			// send()
+			break;
+		case 3:
+			//move
+			//send()
+			break;
+		default:
+			break;
+		}
+
+
+
 		//Buffer 직렬화 ? 
 		send(ClientSocket, (char*)&mypack, sizeof(mypack), 0);
 		cout << "Send Once" << endl;
+
+
 	}
 
 	return 0;
